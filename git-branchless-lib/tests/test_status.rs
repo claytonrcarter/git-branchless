040da44b9bddc5973f41fa48f0acf7323207111c
Clayton Carter <claytonrcarter@gmail.com>
2026-05-19 09:57:22 -0400

    test: implement review feedback

--- git-branchless-lib/tests/test_status.rs
+++ git-branchless-lib/tests/test_status.rs
@@ -290,53 +290,63 @@ fn test_get_status_with_dirty_submodule() -> eyre::Result<()> {
         make_initial_commit: true,
         run_branchless_init: false,
     })?;
-
-    let submodule_source_path = git.repo_path.join("submodule-source");
-    fs::create_dir(&submodule_source_path)?;
-    let submodule_source_path_str = submodule_source_path
-        .to_str()
-        .expect("submodule source path should be UTF-8");
-    git.run(&["-C", submodule_source_path_str, "init"])?;
-    git.run(&[
-        "-C",
-        submodule_source_path_str,
-        "config",
-        "user.name",
-        "Testy McTestface",
-    ])?;
-    git.run(&[
-        "-C",
-        submodule_source_path_str,
-        "config",
-        "user.email",
-        "test@example.com",
-    ])?;
-    fs::write(submodule_source_path.join("file.txt"), "contents\n")?;
-    git.run(&["-C", submodule_source_path_str, "add", "file.txt"])?;
-    git.run(&["-C", submodule_source_path_str, "commit", "-m", "initial"])?;
-
-    git.run_with_options(
-        &[
-            "-c",
-            "protocol.file.allow=always",
-            "submodule",
-            "add",
-            submodule_source_path_str,
-            "sm",
-        ],
-        &GitRunOptions::default(),
-    )?;
-    git.run(&["add", ".gitmodules", "sm"])?;
-    git.run(&["commit", "-m", "add submodule"])?;
-
-    git.run(&["-C", "sm", "config", "user.name", "Testy McTestface"])?;
-    git.run(&["-C", "sm", "config", "user.email", "test@example.com"])?;
-    fs::write(git.repo_path.join("sm/file.txt"), "updated\n")?;
-    git.run(&["-C", "sm", "commit", "-am", "update"])?;
-
     let glyphs = Glyphs::text();
     let effects = Effects::new_suppress_for_test(glyphs);
     let repo = git.get_repo()?;
+
+    let submodule_source_path = git.repo_path.join("submodule-source");
+    let submodule_source_path_str = submodule_source_path
+        .to_str()
+        .expect("submodule source path should be UTF-8");
+
+    {
+        // create repo for submodule
+        fs::create_dir(&submodule_source_path)?;
+        git.run(&["-C", submodule_source_path_str, "init"])?;
+        git.run(&[
+            "-C",
+            submodule_source_path_str,
+            "config",
+            "user.name",
+            "Testy McTestface",
+        ])?;
+        git.run(&[
+            "-C",
+            submodule_source_path_str,
+            "config",
+            "user.email",
+            "test@example.com",
+        ])?;
+        fs::write(submodule_source_path.join("file.txt"), "contents\n")?;
+        git.run(&["-C", submodule_source_path_str, "add", "file.txt"])?;
+        git.run(&["-C", submodule_source_path_str, "commit", "-m", "initial"])?;
+    }
+
+    {
+        // add submodule to main repo
+        git.run_with_options(
+            &[
+                "-c",
+                "protocol.file.allow=always",
+                "submodule",
+                "add",
+                submodule_source_path_str,
+                "sm",
+            ],
+            &GitRunOptions::default(),
+        )?;
+        git.run(&["add", ".gitmodules", "sm"])?;
+        git.run(&["commit", "-m", "add submodule"])?;
+    }
+
+    {
+        // make change in submodule
+        git.run(&["-C", "sm", "config", "user.name", "Testy McTestface"])?;
+        git.run(&["-C", "sm", "config", "user.email", "test@example.com"])?;
+        fs::write(git.repo_path.join("sm/file.txt"), "updated\n")?;
+        git.run(&["-C", "sm", "commit", "-am", "update"])?;
+    }
+
     let (_snapshot, status) = repo.get_status(
         &effects,
         &git_run_info,
@@ -345,9 +355,18 @@ fn test_get_status_with_dirty_submodule() -> eyre::Result<()> {
         None,
     )?;
 
-    assert!(status.iter().any(|entry| {
-        entry.path == PathBuf::from("sm") && entry.working_copy_file_mode == FileMode::Commit
-    }));
+    // Should contain a single entry for the modified submodule.
+    insta::assert_debug_snapshot!(status, @r###"
+        [
+            StatusEntry {
+                index_status: Unmodified,
+                working_copy_status: Modified,
+                working_copy_file_mode: Commit,
+                path: "sm",
+                orig_path: None,
+            },
+        ]
+    "###);
 
     Ok(())
 }
@@ -361,15 +380,17 @@ fn test_get_status_with_directory_symlink_trailing_slash() -> eyre::Result<()> {
         make_initial_commit: false,
         run_branchless_init: false,
     })?;
-
-    fs::create_dir(git.repo_path.join("dir"))?;
-    fs::write(git.repo_path.join("dir/file.txt"), "contents\n")?;
-    std::os::unix::fs::symlink("dir/", git.repo_path.join("dir_symlink"))?;
-    git.run(&["add", "dir/file.txt", "dir_symlink"])?;
-
     let glyphs = Glyphs::text();
     let effects = Effects::new_suppress_for_test(glyphs);
     let repo = git.get_repo()?;
+
+    {
+        fs::create_dir(git.repo_path.join("dir"))?;
+        fs::write(git.repo_path.join("dir/file.txt"), "contents\n")?;
+        std::os::unix::fs::symlink("dir/", git.repo_path.join("dir_symlink"))?;
+        git.run(&["add", "dir/file.txt", "dir_symlink"])?;
+    }
+
     let (_snapshot, status) = repo.get_status(
         &effects,
         &git_run_info,
@@ -378,9 +399,26 @@ fn test_get_status_with_directory_symlink_trailing_slash() -> eyre::Result<()> {
         None,
     )?;
 
-    assert!(status.iter().any(|entry| {
-        entry.path == PathBuf::from("dir_symlink") && entry.working_copy_file_mode == FileMode::Link
-    }));
+    // Should contain new (added) entries for 2 files: dir/file.txt (blob) and
+    // dir_symlink (link)
+    insta::assert_debug_snapshot!(status, @r###"
+        [
+            StatusEntry {
+                index_status: Added,
+                working_copy_status: Unmodified,
+                working_copy_file_mode: Blob,
+                path: "dir/file.txt",
+                orig_path: None,
+            },
+            StatusEntry {
+                index_status: Added,
+                working_copy_status: Unmodified,
+                working_copy_file_mode: Link,
+                path: "dir_symlink",
+                orig_path: None,
+            },
+        ]
+    "###);
 
     Ok(())
 }
